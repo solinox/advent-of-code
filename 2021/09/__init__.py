556a9f89d54d2210da555a045f46f48340a628f7
solinox <bosch@DESKTOP-V24UC7E.localdomain>
2021-12-09 12:37:32 -0500

    day 9 cleanup

--- 2021/09/__init__.py
+++ 2021/09/__init__.py
@@ -1,27 +1,24 @@
 def parse(f):
     return [[int(c) for c in x.strip()] for x in f]
 
-def is_low(data, x, y, mx, my, ):
-    pt = data[y][x]
-    if pt == 9:
+def is_low(data, pt):
+    x, y = pt
+    v = data[y][x]
+    if v == 9:
         return False
-    if x > 0 and pt >= data[y][x-1]:
-        return False
-    if x < mx and pt >= data[y][x+1]:
-        return False
-    if y > 0 and pt >= data[y-1][x]:
-        return False
-    if y < my and pt >= data[y+1][x]:
-        return False
+    for pt in [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]:
+        xx, yy = pt
+        if yy < 0 or yy >= len(data) or xx < 0 or xx >= len(data[yy]):
+            continue
+        if v > data[yy][xx]:
+            return False
     return True
 
 def low_points(data):
-    maxy = len(data)
     lows, lows_locs = [], []
-    for y in range(maxy):
-        maxx = len(data[y])
-        for x in range(maxx):
-            if is_low(data, x, y, maxx-1, maxy-1):
+    for y in range(len(data)):
+        for x in range(len(data[y])):
+            if is_low(data, (x, y)):
                 lows.append(data[y][x])
                 lows_locs.append((x, y))
     return lows, lows_locs
@@ -38,7 +35,7 @@ def explore_basin(data, explored):
         xx, yy = pt
         if yy < 0 or yy >= len(data) or xx < 0 or xx >= len(data[yy]):
             continue
-        if data[pt[1]][pt[0]] != 9:
+        if data[yy][xx] != 9:
             explored.append(pt)
             explored = explore_basin(data, explored)
     return explored
