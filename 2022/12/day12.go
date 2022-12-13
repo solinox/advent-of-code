bc87fcb4e74fc17849dd19fd0f3d9164bfb5d7ee
solinox <abosch.dev@gmail.com>
2022-12-12 19:26:22 -0500

    2022 day 12 (optimize)

--- 2022/12/day12.go
+++ 2022/12/day12.go
@@ -40,65 +40,51 @@ func parse(input []byte) HeightMap {
 	return hm
 }
 
-func astar(g [][]byte, start, end Vector, prevSolved map[Vector]int) int {
-	openSet := map[Vector]struct{}{start: {}}
-	cameFrom := map[Vector]Vector{}
-	gScore := map[Vector]int{start: 0}
-	fScore := map[Vector]int{start: end.Dist(start)}
+// simplified some pieces to optimize the solution so this isn't a true A* algo anymore, sort of a bfs+A* hybrid
+func astar(g [][]byte, start Vector, check func(v Vector) bool) int {
+	openSet := map[Vector]int{start: 0}
+	visited := map[Vector]bool{start: true}
 
-	// stores min steps from each node along the path, and returns the steps from start to end
-	reconstructPath := func(cameFrom map[Vector]Vector, current Vector, prevSolved map[Vector]int, v int) []Vector {
-		path := []Vector{current}
-		for n, ok := cameFrom[current]; ok; n, ok = cameFrom[n] {
-			path = append(path, n)
-			prevSolved[n] = len(path) - 1 + v
-		}
-		return path
-	}
-
-	min := func(openSet map[Vector]struct{}, fScore map[Vector]int) Vector {
+	min := func(openSet map[Vector]int) (Vector, int) {
 		min := math.MaxInt
 		minV := Vector{}
-		for k := range openSet {
-			if s := fScore[k]; s < min {
-				min = s
+		for k, v := range openSet {
+			if v < min {
+				min = v
 				minV = k
 			}
 		}
-		return minV
+		return minV, min
 	}
 
 	for len(openSet) > 0 {
-		current := min(openSet, fScore)
-		if current == end {
-			return len(reconstructPath(cameFrom, current, prevSolved, 0)) - 1
+		current, cost := min(openSet)
+		if check(current) {
+			return cost
 		}
-		if v, ok := prevSolved[current]; ok && v > 0 {
-			// the minimal path from the current node has already been solved
-			return len(reconstructPath(cameFrom, current, prevSolved, v)) - 1 + v
-		}
+		visited[current] = true
 		delete(openSet, current)
 		for _, n := range []Vector{
-			current.Add(Vector{1, 0}),
-			current.Add(Vector{-1, 0}),
-			current.Add(Vector{0, 1}),
-			current.Add(Vector{0, -1}),
+			{Y: current.Y + 1, X: current.X},
+			{Y: current.Y - 1, X: current.X},
+			{Y: current.Y, X: current.X + 1},
+			{Y: current.Y, X: current.X - 1},
 		} {
 			if n.X < 0 || n.Y < 0 || n.Y >= len(g) || n.X >= len(g[n.Y]) {
 				// out of bounds
 				continue
 			}
-			if int(g[n.Y][n.X])-int(g[current.Y][current.X]) > 1 {
+			if visited[n] {
+				// already visited
+				continue
+			}
+			// going downhill instead of uphill
+			if int(g[current.Y][current.X])-int(g[n.Y][n.X]) > 1 {
 				// cannot climb that fast
 				continue
 			}
-			tentativeG := gScore[current] + 1
-			if g, ok := gScore[n]; !ok || tentativeG < g {
-				gScore[n] = tentativeG
-				fScore[n] = tentativeG + end.Dist(n)
-				cameFrom[n] = current
-				openSet[n] = struct{}{}
-			}
+			cost := cost + 1
+			openSet[n] = cost
 		}
 	}
 
@@ -106,22 +92,9 @@ func astar(g [][]byte, start, end Vector, prevSolved map[Vector]int) int {
 }
 
 func part1(hm HeightMap) int {
-	return astar(hm.Grid, hm.Start, hm.End, make(map[Vector]int))
+	return astar(hm.Grid, hm.End, func(v Vector) bool { return v == hm.Start })
 }
 
 func part2(hm HeightMap) int {
-	min := math.MaxInt
-	prevSolved := make(map[Vector]int)
-	for y := 0; y < len(hm.Grid); y++ {
-		for x := 0; x < len(hm.Grid[y]); x++ {
-			if hm.Grid[y][x] == 'a' {
-				start := Vector{Y: y, X: x}
-				v := astar(hm.Grid, start, hm.End, prevSolved)
-				if v > 0 && v < min {
-					min = v
-				}
-			}
-		}
-	}
-	return min
+	return astar(hm.Grid, hm.End, func(v Vector) bool { return hm.Grid[v.Y][v.X] == 'a' })
 }
