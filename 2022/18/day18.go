7cbf0194d6b3db73ce70b4803ac700046f96994b
solinox <abosch.dev@gmail.com>
2022-12-26 20:41:09 -0500

    2022 day 18 (optimized)

--- 2022/18/day18.go
+++ 2022/18/day18.go
@@ -3,6 +3,7 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"math"
 	"strings"
 
 	"github.com/solinox/advent-of-code/2022/util"
@@ -24,6 +25,7 @@ func main() {
 		return cubes
 	}, make(map[Vector]bool))
 	util.RunTimed(part1, cubes)
+	util.RunTimed(part2Optimized, cubes)
 	util.RunTimed(part2, cubes)
 }
 
@@ -39,6 +41,54 @@ func part1(cubes map[Vector]bool) int {
 	return surfaceArea
 }
 
+// get bounding cube to cover all cubes, then flood fill from one corner
+// and count all the surfaces that flooding comes in contact with
+func part2Optimized(cubes map[Vector]bool) int {
+	surfaceArea := 0
+	minX, maxX, minY, maxY, minZ, maxZ := math.MaxInt, math.MinInt, math.MaxInt, math.MinInt, math.MaxInt, math.MinInt
+	for c := range cubes {
+		if c.X <= minX {
+			minX = c.X - 1
+		}
+		if c.X >= maxX {
+			maxX = c.X + 1
+		}
+		if c.Y <= minY {
+			minY = c.Y - 1
+		}
+		if c.Y >= maxY {
+			maxY = c.Y + 1
+		}
+		if c.Z <= minZ {
+			minZ = c.Z - 1
+		}
+		if c.Z >= maxZ {
+			maxZ = c.Z + 1
+		}
+	}
+	start := Vector{Z: minZ, Y: minY, X: minX}
+	checked := map[Vector]bool{start: true}
+	cells := []Vector{start}
+	for len(cells) > 0 {
+		c := cells[0]
+		cells = cells[1:]
+		for _, d := range dirs {
+			cd := c.Add(d)
+			if cubes[cd] {
+				surfaceArea++
+			} else if cd.Z >= minZ && cd.Z <= maxZ && cd.Y >= minY && cd.Y <= maxY && cd.X >= minX && cd.X <= maxX && !checked[cd] {
+				cells = append(cells, cd)
+				checked[cd] = true
+			}
+		}
+	}
+	return surfaceArea
+}
+
+// for each cube, check if cube face is an exterior face
+// by getting min/max values for each direction (xy/yz/xz)
+// unfortunately there are some edge cases which cause us to have to recursively search neighbors
+// to confirm if an open face is exterior or not
 func part2(cubes map[Vector]bool) int {
 	surfaceArea := 0
 	ranges := make(map[Vector]Range)
